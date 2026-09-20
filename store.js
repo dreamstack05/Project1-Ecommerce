5002e7e0be6586b130f4d61b2c71997751d18878
Priya05 <271773513+dreamstack05@users.noreply.github.com>
2026-09-20 19:54:10 +0530

    Update store.js

--- store.js
+++ store.js
@@ -1,6 +1,6 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux';
 import {thunk} from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import { productCreateReducers, productDeleteReducers, productDetailsReducers, productListReducers, productUpdateReducers } from './reducers/productReducers';
 import { userDeleteReducer, userDetailsReducer, userListReducer, userLoginReducers, userSignupReducers, userUpdateProfileReducer, userUpdateReducer } from './reducers/userReducers';
 import { cartReducers } from './reducers/cartReducers';
